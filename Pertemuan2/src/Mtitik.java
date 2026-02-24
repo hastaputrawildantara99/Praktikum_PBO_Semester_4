97b1a6c56636d6e66f91a9637d6104e3266cd851
hastaputrawildantara99 <hastaputrawildantara@gmail.com>
2026-02-24 15:56:35 +0700

    Finisihing Pertemuan2

--- Pertemuan2/src/Mtitik.java
+++ Pertemuan2/src/Mtitik.java
@@ -1,4 +1,4 @@
-// Nama File : Titik.java
+// Nama File : Mtitik.java
 // Deskripsi : berisi atribut dan method dalam class titik 
 // Pembuat   : Hasta Putra Wildantara / 24060124130119
 // Tanggal   : 2/24/2026
@@ -6,19 +6,32 @@
 public class Mtitik {
     public static void main(String[] args) {
         Titik t1 = new Titik();
+        System.out.println("Titik t1: ");
         t1.printTitik();
+        System.out.println("\nSet titik t1 menjadi x=2, y=3: ");
         t1.setAbsis(2);
         t1.setOrdinat(3);
         t1.printTitik();
-        t1.geser(3, 4);
+        System.out.println("\nGeser titik t1 1 langkah ke kanan dan ke atas: ");
+        t1.geser(1, 1);
         t1.printTitik();
+        System.out.println("\nKuadran t1: " + t1.getKuadran());
+        System.out.println("Jarak pusat ke t1: " + t1.getJarakPusat());
 
-        Titik T2 = t1;
+        Titik T2 = new Titik(9, 12);
+        System.out.println("\nTitik T2: ");
         T2.printTitik();
-        t1.setAbsis(10);
-        t1.setOrdinat(10);
-        T2.printTitik();
-        t1.getKuadran();
-        System.out.println(Titik.getCounterTitik());
+        System.out.println("\nJarak t1 ke T2: " + t1.getJarak(T2));
+        System.out.println("\nRefleksi Titik t1 terhadap X");
+        t1.refleksiX();
+        t1.printTitik();
+        System.out.println("\nRefleksi Titik t1 terhadap Y");
+        t1.refleksiY();
+        t1.printTitik();
+        System.out.println("\nFungsi Refleksi X t1 (Titik baru): ");
+        t1.getRefleksiX().printTitik();
+        System.out.println("\nFungsi Refleksi Y t1 (Titik Baru): ");
+        t1.getRefleksiY().printTitik();
+        System.out.println("\nJumlah Titik: " + Titik.getCounterTitik());
     }
 }
--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -3,63 +3,91 @@
 // Pembuat   : Hasta Putra Wildantara / 24060124130119
 // Tanggal   : 2/24/2026
 
-public class Titik{
+public class Titik {
     // Atribut
     double absis;
     double ordinat;
-    static int counterTitik = 0; 
+    static int counterTitik = 0;
 
     // Method
-    Titik(){
+    Titik() {
         absis = 0;
         ordinat = 0;
         counterTitik++;
     }
 
-    Titik(double absis, double ordinat){
+    Titik(double absis, double ordinat) {
         this.absis = absis;
         this.ordinat = ordinat;
         counterTitik++;
     }
 
-    static int getCounterTitik(){
+    static int getCounterTitik() {
         return counterTitik;
     }
 
-    double getAbsis(){
+    double getAbsis() {
         return absis;
     }
 
-    double getOrdinat(){
+    double getOrdinat() {
         return ordinat;
     }
 
-    void setAbsis(double a){
+    void setAbsis(double a) {
         absis = a;
     }
 
-    void setOrdinat(double o){
+    void setOrdinat(double o) {
         ordinat = o;
     }
 
-    void geser(double a, double o){
+    void geser(double a, double o) {
         absis = absis + a;
         ordinat = ordinat + o;
     }
 
-    int getKuadran(){
-        if(absis>0 && ordinat>0){
+    int getKuadran() {
+        if (absis > 0 && ordinat > 0) {
             return 1;
-        } else if(absis<0 && ordinat>0){
+        } else if (absis < 0 && ordinat > 0) {
             return 2;
-        } else if(absis<0 && ordinat<0){
+        } else if (absis < 0 && ordinat < 0) {
             return 3;
-        } else if(absis>0 && ordinat<0){
+        } else if (absis > 0 && ordinat < 0) {
             return 4;
-        } else{
+        } else {
             return 0;
         }
     }
+
+    double getJarakPusat() {
+        return Math.sqrt(absis * absis + ordinat * ordinat);
+    }
+
+    double getJarak(Titik t) {
+        double dx = this.absis - t.absis;
+        double dy = this.ordinat - t.ordinat;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    void refleksiX() {
+        ordinat = -ordinat;
+    }
+
+    void refleksiY() {
+        absis = -absis;
+    }
+
+    Titik getRefleksiX() {
+        return new Titik(-absis, ordinat);
+    }
+
+    Titik getRefleksiY() {
+        return new Titik(absis, -ordinat);
+    }
+
+     
 
     void printTitik(){
         System.out.println("Titik (" + absis + ", " + ordinat + ")");
