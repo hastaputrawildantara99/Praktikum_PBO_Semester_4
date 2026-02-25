f21e5c1e75c3822e09f2bec869494e61e766edcf
hastaputrawildantara99 <hastaputrawildantara@gmail.com>
2026-02-25 21:00:35 +0700

    Tugas Pertemuan2

---
+++ Pertemuan2/src/Garis.java
@@ -0,0 +1,69 @@
+public class Garis {
+    // Atribut
+    Titik titikAwal;
+    Titik titikAkhir;
+    static int counterGaris = 0;
+
+    // Method
+    Garis() {
+        titikAwal = new Titik(0, 0);
+        titikAkhir = new Titik(1, 1);
+        counterGaris++;
+    }
+
+    Garis(Titik t1, Titik t2) {
+        titikAwal = t1;
+        titikAkhir = t2;
+        counterGaris++;
+    }
+
+    Titik getTitikAwal() {
+        return titikAwal;
+    }
+
+    Titik getTitikAkhir() {
+        return titikAkhir;
+    }
+
+    void setTitikAwal(Titik t1) {
+        titikAwal = t1;
+    }
+
+    void setTitikAkhir(Titik t2) {
+        titikAkhir = t2;
+    }
+
+    static int getCounterGaris() {
+        return counterGaris;
+    }
+
+    double PanjangGaris() {
+        return Math.sqrt(Math.pow(titikAkhir.getAbsis() - titikAwal.getAbsis(), 2)
+                + Math.pow(titikAkhir.getOrdinat() - titikAwal.getOrdinat(), 2));
+    }
+
+    double GradienGaris() {
+        return (titikAkhir.getOrdinat() - titikAwal.getOrdinat()) / (titikAkhir.getAbsis() - titikAwal.getAbsis());
+    }
+
+    Titik getTitikTengah() {
+        double absis = (titikAwal.getAbsis() + titikAkhir.getAbsis()) / 2;
+        double ordinat = (titikAwal.getOrdinat() + titikAkhir.getOrdinat()) / 2;
+        return new Titik(absis, ordinat);
+    }
+
+    boolean isSejajar(Garis g) {
+        return this.GradienGaris() == g.GradienGaris();
+    }
+
+    void printGaris() {
+        System.out.println("Garis (" + titikAwal.getAbsis() + ", " + titikAwal.getOrdinat() + ") -> ("
+                + titikAkhir.getAbsis() + ", " + titikAkhir.getOrdinat() + ")");
+    }
+
+    String getPersamaanGaris() {
+        double m = GradienGaris();
+        double c = titikAwal.getOrdinat() - m * titikAwal.getAbsis();
+        return "y = " + m + "x + " + c;
+    }
+}
---
+++ Pertemuan2/src/MGaris.java
@@ -0,0 +1,19 @@
+public class MGaris {
+    public static void main(String[] args) {
+        Garis g1 = new Garis();
+        Garis g2 = new Garis(new Titik(1, 2), new Titik(3, 4));
+        g1.printGaris();
+        g2.printGaris();
+        System.out.println("Panjang g1: " + g1.PanjangGaris());
+        System.out.println("Panjang g2: " + g2.PanjangGaris());
+        System.out.println("Gradien g1: " + g1.GradienGaris());
+        System.out.println("Gradien g2: " + g2.GradienGaris());
+        System.out.println("Titik tengah g1: ");
+        g1.getTitikTengah().printTitik();
+        System.out.println("Titik tengah g2: ");
+        g2.getTitikTengah().printTitik();
+        System.out.println("Garis g1 dan g2 sejajar: " + g1.isSejajar(g2));
+        System.out.println("Persamaan g1: " + g1.getPersamaanGaris());
+        System.out.println("Persamaan g2: " + g2.getPersamaanGaris());
+    }
+}
